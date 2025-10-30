a9aad1d4caeba8678097363e8a56d31fd1c23a2a
Lucas Oliveira <lucasolisociamedia@gmail.com>
2025-10-30 10:38:53 -0300

    Adicionando perfil de testes

--- src/test/java/com/jenkins/apiwithjenkins/ApiWithJenkinsApplicationTests.java
+++ src/test/java/com/jenkins/apiwithjenkins/ApiWithJenkinsApplicationTests.java
@@ -1,13 +1,16 @@
 package com.jenkins.apiwithjenkins;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.profiles.active=test")
 class ApiWithJenkinsApplicationTests {
 
     @Test
     void contextLoads() {
+        assertTrue(true);
     }
 
 }
