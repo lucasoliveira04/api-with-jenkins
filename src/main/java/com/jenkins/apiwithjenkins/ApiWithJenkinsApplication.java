f1d5a39a87ebeb1a662f9b63edf1df315098b838
Lucas Oliveira <lucasolisociamedia@gmail.com>
2025-10-30 09:52:05 -0300

    init

---
+++ src/main/java/com/jenkins/apiwithjenkins/ApiWithJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.apiwithjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWithJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiWithJenkinsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/jenkins/apiwithjenkins/controller/AppController.java
@@ -0,0 +1,25 @@
+package com.jenkins.apiwithjenkins.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@Profile("dev")
+public class AppController {
+
+    @GetMapping("/list-users")
+    public ResponseEntity<List<String>> listUsers() {
+        List<String> list = new ArrayList<>();
+        list.add("Jenkins");
+        list.add("Jenkins");
+        return ResponseEntity.ok(list);
+    }
+}
