c99d3dd431994d76df62915248fc2dc51482973d
Lucas Oliveira <lucasolisociamedia@gmail.com>
2025-10-31 15:11:33 -0300

    mudando nome do controller

--- src/main/java/com/jenkins/apiwithjenkins/controller/AppController.java
+++ src/main/java/com/jenkins/apiwithjenkins/controller/AppController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Profile("dev")
 public class AppController {
 
-    @GetMapping("/list")
+    @GetMapping("/list-user")
     public ResponseEntity<List<String>> listUsers() {
         List<String> list = new ArrayList<>();
         list.add("Jenkins");
